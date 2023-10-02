65174d97ddf7770668ffe7cae6e0d412792e53c9
PedroRiccetto <113381414+PedroRiccetto@users.noreply.github.com>
2023-10-02 11:11:31 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,17 +4,17 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Prof. Massaki Igarashi")
+st.title("Pedro Riccetto")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Teste de Cabeçalho")
+st.header("Roupas importadas")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Como já deve ter percebido, so marca de qualidade e fina!")
 
-st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.info("Como já deve ter percebido, so marca de qualidade e fina!")
 
-st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.warning("Como já deve ter percebido, so marca de qualidade e fina!")
